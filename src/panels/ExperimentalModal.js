e2073db1c8796333dc84079e6e28118452a77cc7
crutchm <sads1100@gmail.com>
2022-12-18 14:28:35 +0500

    scored balance

--- src/panels/ExperimentalModal.js
+++ src/panels/ExperimentalModal.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import {Avatar, ModalCard} from '@vkontakte/vkui';
 
 import persik from '../img/persik.png';
 import './Persik.css';
+import {localStorage} from "@vkontakte/vkjs";
 
 
 const ExperimentalModal = props => {
+	const [balance, setBalance] = useState(0)
+	useEffect(()=>{
+		let tmp = localStorage.getItem('score')
+		if (tmp === 'null'){
+			tmp = 0
+		}
+		localStorage.setItem("score", (Number(tmp) + 1).toString())
+		setBalance(Number(localStorage.getItem("score")))
+	},[])
 
 	const onButtonClicked = () => {
 		props.go('persik')
 		props.changeActiveModal(null)
 	}
-
 	return <ModalCard
 		id={props.id}
 		onClose={() => props.changeActiveModal(null)}
 		data-to="home"
 		icon={<Avatar src={props.fetchedUser.photo_200}/>}
-		header={`Ваш счет: ${props.balance}`}
-	></ModalCard>
+		header={`Ваш счет: ${balance}`}></ModalCard>
+
 };
 
 ExperimentalModal.propTypes = {
